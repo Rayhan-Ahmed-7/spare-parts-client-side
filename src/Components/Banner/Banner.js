a720782506110afec843ce8eb338642ff96dd1e0
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2022-05-23 15:13:22 +0600

    header added

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import Header from '../Header/Header';
+import { MdArrowForwardIos } from 'react-icons/md';
 //import bgImg from '../../images/banner.png';
 
 const Banner = () => {
     return (
         <div className="min-h-screen bg-banner-image bg-cover">
             <Header></Header>
-            <div className='w-10/12 mx-auto flex min-h-[80vh] items-center'>
+            <div className='lg:w-10/12 w-11/12 mx-auto flex min-h-[100vh] items-center'>
                 <div className='text-white flex-1'>
-                    <small className='tracking-[1rem]'>SPARE PARTS</small>
-                    <h2 className='lg:text-5xl text-2xl font-bold mt-4 font-title tracking-[.2rem]'>WE PROVIDE PREMIUM<br /><span className='text-primary'> QUALITY CAR SPARE PARTS</span></h2>
-                    <p className='py-4 text-justify'>
+                    <small className='lg:tracking-[1rem] tracking-[.5rem]'>SPARE PARTS</small>
+                    <h2 className='lg:text-5xl text-xl font-bold lg:mt-4 mt-2 font-title tracking-[.2rem]'>WE PROVIDE PREMIUM<br /><span className='text-primary'> QUALITY CAR SPARE PARTS</span></h2>
+                    <p className='lg:py-4 py-2 lg:text-[14px] text-[12px] font-light text-gray-200'>
                         We offer spare parts Our company provides its customers with not only high quality products and product designs, but also offers a wide range of individual parts. Sometimes, a needed separate part is harder to find than get the whole new product. Nowadays Technologies takes care of the needs of our customers, so we are ready to help you out completely.
                     </p>
-                    <button className='bg-primary px-4 py-2'>Check Now</button>
+                    <button className='bg-primary button px-4 py-2 flex items-center rounded-sm'>Check Now <MdArrowForwardIos className='icon ml-2 transition-all'/></button>
                 </div>
                 <div className='flex-1 lg:block hidden'></div>
             </div>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../images/logo.png';
+import { HiMenuAlt1 } from 'react-icons/hi';
+import { CgClose } from 'react-icons/cg';
+import { Link } from 'react-router-dom';
 const Header = () => {
+    const [menuOpen,setMenuOpen] = useState(false);
+    const [profileOpen,setProfileOpen] = useState(false);
+    
     //FFCB00 = yellow
     return (
-        <header className='bg-[#383264] h-20 flex items-center bg-opacity-40 border-b border-b-gray-700'>
-            <div className='w-11/12 mx-auto'>
+        <header className='bg-[#383264] h-20 flex items-center bg-opacity-40 border-b border-b-gray-700 backdrop-blur-sm sticky top-0 z-50'>
+            <div className='w-11/12 mx-auto flex items-center justify-between relative'>
                 <div className="logo">
-                    <img src={logo} alt="logo" />
+                    <img className='lg:h-auto h-8' src={logo} alt="logo" />
                 </div>
-                <nav>
-
-                </nav>
+                <div className='lg:hidden block'>
+                    <button onClick={()=>{setMenuOpen(!menuOpen)}}>
+                        {
+                            menuOpen ? 
+                            <CgClose className='text-3xl text-white' />
+                            :
+                            <HiMenuAlt1 className='text-3xl text-white' />
+                        }
+                    </button>
+                </div>
+                <ul className={`${menuOpen? 'opacity-100 translate-y-0 scale-100':'opacity-0 -translate-y-20 scale-0'} lg:scale-100 lg:translate-y-0 lg:opacity-100 transition-all duration-300 flex lg:flex-row flex-col items-center lg:space-x-4 space-x-0 lg:space-y-0 space-y-4 text-white lg:static absolute lg:z-0 -z-10 top-[68px] right-0  lg:bg-transparent bg-[#383264] lg:bg-opacity-0 bg-opacity-40 lg:backdrop-blur-0 backdrop-blur-sm lg:w-auto w-44 lg:p-0 p-4 rounded-md`}>
+                    <li>
+                        <Link to=''>Home</Link>
+                    </li>
+                    <li>
+                        <Link to=''>Car Parts</Link>
+                    </li>
+                    <li>
+                        <Link to=''>Reviews</Link>
+                    </li>
+                    <li>
+                        <Link to=''>Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link to=''>Login</Link>
+                    </li>
+                    <li className='relative'>
+                        <img onClick={()=>setProfileOpen(!profileOpen)} src="https://i.ibb.co/cx5jTVj/dinner5.png" className='h-10 w-10 rounded-full cursor-pointer' alt="profile"/>
+                        <div className={`${profileOpen?'opacity-100':'opacity-0'} absolute top-12 right-0 bg-[#383264] bg-opacity-40 backdrop-blur-sm p-4 w-44 rounded-md space-y-4 z-50`}>
+                            <h2 className='text-sm'>Rayhan Ahmed</h2>
+                            <button className='bg-primary px-2 py-1 rounded-md text-sm'>Log Out</button>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </header>
     );
